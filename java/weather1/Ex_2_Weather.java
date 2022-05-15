467947f1a87ba3b8a9c9bf40fab60d2eed9aa162
Amrudha-19cse002 <68490508+Amrudha-19cse002@users.noreply.github.com>
2022-05-15 20:17:58 +0530

    Add files via upload

---
+++ java/weather1/Ex_2_Weather.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weather1;
+
+/**
+ *
+ * @author Amrudha
+ */
+public class Ex_2_Weather {
+    String city;
+    String maxTemp;
+    String minTemp;
+    String status;
+ public Ex_2_Weather(String city, String maxTemp, String minTemp, String status) {
+        this.city = city;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.status = status;
+    }
+}
